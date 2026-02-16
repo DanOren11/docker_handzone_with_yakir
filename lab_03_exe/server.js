a36ede2b409493d0f79cee40315daf013e337bc0
dan oren <dan.oren@powercom.co.il>
2026-02-16 16:59:28 +0200

    Yakir exc - 03 volumes

---
+++ lab_03_exe/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const log4js = require("log4js");
+log4js.configure({
+  appenders: { myapp: { type: "file", filename: "logs/myapp.log" } },
+  categories: { default: { appenders: ["myapp"], level: "debug" } },
+});
+
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const containerId = process.env.CONTAINER_ID || "app1";
+app.get("/health", (req, res) => {
+
+  const logEntery = {
+    "message": "Health check called",
+    "containerId": containerId,
+    "timestamp": new Date().toISOString()
+  }
+  logger.debug(logEntery);
+  res.json(getHealth());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const logger = log4js.getLogger();
+const getHealth = () => {
+  logger.debug(`FTOM GET HEALTH Health check called ${containerId}`);
+  return {
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: Date.now().toLocaleString(),
+  };
+};
