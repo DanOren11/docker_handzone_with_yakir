6fb43474aa2dcd29b54f7f57809c05ac1b8e0904
dan oren <dan.oren@powercom.co.il>
2026-02-24 09:31:10 +0200

    Adding .dockerignore and fix labrary

--- lab_05_docker-compose/task_docker-compose/server.js
+++ lab_05_docker-compose/task_docker-compose/server.js
@@ -1,11 +1,31 @@
 const express = require('express')
-const app = express()
-
-
 require('dotenv').config()
-const port = process.env.PORT || 3000
+
+
+
+const mysql = require('mysql2');
+
+const app = express()
+const port = process.env.PORT || 8080
+
+const dbConfig = {
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'dan',
+  password: process.env.MYSQL_PASSWORD || 'dan1',
+  database: process.env.MYSQL_DATABASE || 'mydb'
+};
+
+const connection = mysql.createConnection(dbConfig);
+connection.connect(err => {
+  if (err) {
+    console.error('MySQL connection error:', err.message);
+  } else {
+    console.log('Connected to MySQL');
+  }
+});
+
 app.get('/', (req, res) => {
-  res.send('<H1>Lab 05 Docker Compose </H1>')
+  res.send(`<h1>Docker-Compose-Lab</h1><p>running on port ${port}</p>`)
 })
 
 
